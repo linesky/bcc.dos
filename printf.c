2e1f2e7c07542b40e18e152659e8af4931a3e0ef
sky line <32512284+linesky@users.noreply.github.com>
2024-09-19 03:56:07 +0100

    Add files via upload

--- printf.c
+++ printf.c
@@ -2,8 +2,8 @@
 
 #define varn 0xc080
 
-void cls3(cls1);
-void sputc(cc);
+
+
 void sputs(cc);
 void scopy(s1,s2);
 void scat(s1,s2);
@@ -18,11 +18,11 @@ char *argv[];
 	int idx=0;
 	char *NULL;
 	*NULL=0;
-	cls3(0x71);
+	cls3(0x2007);
 	if(argc>1){
 	        char *token=strtok(argv[1],"%");
-	        printf("%s",token);
-		   
+	        sputs(token);
+		sputc(32);   
 		for (n=1;n<argc-1;n++){
 		        
 		        
@@ -33,33 +33,13 @@ char *argv[];
 			sputs(argv[n+1]);
 			token=token+1;
 			sputs(token);
+			sputc(32);
 		}
 		
 	}else{
 		return 1;
 	}
 	return 0;
-}
-void cls3(cls1)
-{
-        int *c;
-	c = (int * ) varn;
-		*(c + 0)=cls1;
-		
-		asm	"db 0x1e,0x3e,0x8b,0x16,0x80,0xc0,0xbb,0x01,0x00,0xb8,0x00,0xb8,0xb9,0xd0,0x07,0x8e,0xd8,0x88,0xd0,0x3e,0x88,0x07,0x43,0x43,0x49,0x83,0xf9,0x00,0x75,0xf5,0x1f";
-		
-	
-
-	
-	}
-void sputc(cc)
-char cc;
-{
-	int *c;
-	c = (int * ) varn;
-	*(c + 0) = cc;
-
-	asm	"db 0xbb,0x80,0xc0,0x8a,0x47,0x0,0xbb,0x7,0x0,0xb4,0x0e,0xcd,0x10";
 }
 
 void sputs(cc)
@@ -70,6 +50,7 @@ while(cc[i]!=0){
 		sputc(cc[i]);
 		i++;
 }
+
 }
 void scopy(s1,s2)
 char *s1;
@@ -112,5 +93,38 @@ char *s1;
 	}while(cc!=0);
 }
 
+#asm
+.globl _cls3
+.globl _sputc 
+_cls3:
+    mov si,sp
+    add si,*0x2
+    mov dx,[si]
+    mov ax,*0xb800
+    push ds
+    mov ds,ax
+    
+    mov ax,dx
+    mov cx,*0x8a0
+    mov si,*0x1
+    
+cls31:
+    
+    mov [si],al    
+    inc si
+    inc si
+    dec cx
+    cmp cx,*0x0
+    jnz cls31
+    pop ds
+    ret
+_sputc:
+    mov si,sp
+    add si,*0x2
+    mov dx,[si]
+    mov ah,*02
+    int *0x21
+    ret
 
+#endasm
 
